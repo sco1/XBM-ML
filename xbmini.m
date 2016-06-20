392d8e9fde594ae555cd9130570fc03bfad63506
S. Co1 <sco1.git@gmail.com>
2016-06-20 10:11:56 -0400

    Add ISO8601 date method

--- xbmini.m
+++ xbmini.m
@@ -25,5 +25,21 @@ classdef xbmini
             dataObj.analysisdate = xbmini.getdate;
         end
     end
+    
+    methods (Static, Access = private)
+        function date = getdate()
+            if ~verLessThan('MATLAB', '8.4')  % datetime added in R2014b
+                timenow = datetime('now', 'TimeZone', 'local');
+                formatstr = sprintf('yyyy-mm-ddTHH:MM:SS%sZ', char(tzoffset(timenow)));
+            else
+                UTCoffset = -java.util.Date().getTimezoneOffset/60;  % See what Java thinks your TZ offset is
+                timenow = clock;
+                formatstr = sprintf('yyyy-mm-ddTHH:MM:SS%i:00Z', UTCoffset);
+            end
+            
+            date = datestr(timenow, formatstr);  % ISO 8601 format
+        end
+    end
+    
 end
 
